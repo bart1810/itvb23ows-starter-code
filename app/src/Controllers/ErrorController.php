88b32b6b185aab0c0bf12d487679dd49152aeff2
bart1810 <barta1810@gmail.com>
2024-01-29 22:54:05 +0100

    Almost done with changing the code to OOP style, need to test move and restart

--- app/src/Controllers/ErrorController.php
+++ app/src/Controllers/ErrorController.php
@@ -8,6 +8,13 @@ class ErrorController {
         return $_SESSION['error'] ?? null;
     }
 
+    public function printError(): void {
+        if (isset($_SESSION['error'])) {
+            echo $this->getError();
+        }
+        $this->unsetError();
+    }
+
     public function setError($error): void {
         $_SESSION['error'] = $error;
     }
--- app/src/Database/Database.php
+++ app/src/Database/Database.php
@@ -14,6 +14,17 @@ class Database {
         return serialize([$_SESSION['hand'], $_SESSION['board'], $_SESSION['player']]);
     }
 
+    public function undo(): void {
+        $lastMove = $this->getLastMove();
+
+        $stmt = $this->database->prepare('SELECT * FROM moves WHERE id = '.$lastMove);
+        $stmt->execute();
+
+        $result = $stmt->get_result()->fetch_array();
+        $this->setLastMove($result[5]);
+        $this->setState($result[6]);
+    }
+
     public function setState($state): void {
         list($a, $b, $c) = unserialize($state);
         $_SESSION['hand'] = $a;
@@ -21,22 +32,64 @@ class Database {
         $_SESSION['player'] = $c;
     }
 
-    public function movePiece($from, $to, $gameId, $lastMove): void {
+    public function getGameId() {
+        return $_SESSION['game_id'] ?? null;
+    }
+
+    public function printMoves(): void {
+        $gameId = $this->getGameId();
+        $stmt = $this->database->prepare('SELECT * FROM moves WHERE game_id = '.$gameId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        while ($row = $result->fetch_array()) {
+            echo '<li>'.$row[2].' '.$row[3].' '.$row[4].'</li>';
+        }
+    }
+
+    public function getLastMove() {
+        return $_SESSION['last_move'] ?? null;
+    }
+
+    public function setLastMove($move): void {
+        $_SESSION['last_move'] = $move;
+    }
+
+    public function passTurn(): void {
         $state = $this->getState();
+        $gameId = $this->getGameId();
+        $lastMove = $this->getLastMove();
+
+        $stmt = $this->database->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "pass", null, null, ?, ?)');
+        $stmt->bind_param('iis', $gameId, $lastMove, $state);
+        $stmt->execute();
+    }
+
+    public function movePiece($from, $to): void {
+        $state = $this->getState();
+        $gameId = $this->getGameId();
+        $lastMove = $this->getLastMove();
+
         $stmt = $this->database->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "move", ?, ?, ?, ?)');
         $stmt->bind_param('issis', $gameId, $from, $to, $lastMove, $state);
         $stmt->execute();
     }
 
-    public function playPiece($to, $piece, $gameId, $lastMove): void {
+    public function playPiece($to, $piece): void {
         $state = $this->getState();
+        $gameId = $this->getGameId();
+        $lastMove = $this->getLastMove();
+
         $stmt = $this->database->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "play", ?, ?, ?, ?)');
-        $stmt->bind_param('issis', $gameId, $piece, $to, $lastMove, $state);
+        $stmt->bind_param('issis', $gameId, $to, $piece, $lastMove, $state);
         $stmt->execute();
     }
 
     public function getInsertID(): int|string {
         return $this->database->insert_id;
+    }
+
+    public function setGameId($gameId): void {
+        $_SESSION['game_id'] = $gameId;
     }
 
     public function restartGame(): void {
@@ -48,5 +101,8 @@ class Database {
         unset($_SESSION['last_move']);
 
         $this->database->prepare('INSERT INTO games VALUES ()')->execute();
+
+        $this->setGameId($this->getInsertID());
+
     }
 }
